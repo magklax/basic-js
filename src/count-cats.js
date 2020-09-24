b2112016d1be04d0e98d89663c494374d5b569b4
Nadia <nadic206@yandex.ru>
2020-09-24 10:05:38 +0200

    init: add solutions for carbon-dating, count-cats and dream-team

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,15 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(backyard) {
+  let number = 0;
+  
+  backyard.forEach(function (array) {
+    array.forEach(function (elem) {
+      if (elem === '^^') {
+        number++;
+      }
+    })
+  });
+  
+  return number;
 };

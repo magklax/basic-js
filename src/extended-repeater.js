bdd02844f9d8e3567565f9d0a379f646ecd4a7d6
Nadia <nadic206@yandex.ru>
2020-09-25 08:28:11 +0200

    feat: add all task solutions

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+	const { 
+    repeatTimes = 1, 
+    separator = '+', 
+    addition ='', 
+    additionRepeatTimes = 1, 
+    additionSeparator = '|' 
+  } = options;
+
+  let result = '';
+  for (let j = 0; j < additionRepeatTimes; j++) {
+    str += String(addition);
+
+    if (j < additionRepeatTimes - 1) {
+      str += additionSeparator;
+    }
+  }
+
+  if (repeatTimes) {
+    for (let i = 0; i < repeatTimes; i++) {
+      result += String(str) ;
+      
+      if (i < repeatTimes - 1) {
+        result += separator;
+      }
+    }
+  }
+  
+  return result;
 };
   
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,67 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(type = true) {
+    this.type = type;
+  }
+
+  encrypt(message, key) {
+    const ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const UNICODE = 65;
+    let encryptedWord = '';
+    let j = 0;
+
+    if (message === undefined || key === undefined) {
+      throw new Error();
+    }
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    for (let i = 0; i < message.length; i++) {
+      if (message[i].match(/^[A-Z]$/gi)) {
+        let charNumber = message[i].charCodeAt() + key[j].charCodeAt() - 2 * UNICODE;
+
+        (charNumber > ALPHABET.length - 1) ? charNumber -= ALPHABET.length : charNumber;
+
+        encryptedWord += ALPHABET.charAt(charNumber);
+
+        (j < key.length - 1) ? j++ : j = 0;
+      } else {
+        encryptedWord += message[i];
+      }
+    }
+
+    return this.type ? encryptedWord : encryptedWord.split('').reverse().join('');
+  }
+
+  decrypt(message, key) {
+    const ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let decryptedWord = '';
+    let j = 0;
+
+    if (message === undefined || key === undefined) {
+      throw new Error();
+    }
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    for (let i = 0; i < message.length; i++) {
+      if (message[i].match(/^[A-Z]$/gi)) {
+        let charNumber = message[i].charCodeAt() - key[j].charCodeAt();
+
+        (charNumber < 0) ? charNumber += ALPHABET.length : charNumber;
+
+        decryptedWord += ALPHABET.charAt(charNumber);
+
+        (j < key.length - 1) ? j++ : j = 0;
+      } else {
+        decryptedWord += message[i];
+      }
+    }
+
+    return this.type ? decryptedWord : decryptedWord.split('').reverse().join('');
   }
 }
 
